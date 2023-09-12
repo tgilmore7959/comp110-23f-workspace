d7d2dd08a4ea59ed59621443c9c5981e0c1814c0
Tom Gilmore <tgilmore@unc.edu>
2023-09-12 19:37:20 -0400

    Chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,4 +1,4 @@
-"""EX01 - Chardle - A cute step toward Wordle"""
+"""EX01 - Chardle - A cute step toward Wordle."""
 __author__ = "730585444"
 word: str = input("Enter a 5-character word: ")
 if (len(word)) != 5:
@@ -9,25 +9,25 @@ if len(single) != 1:
     print("Error: Character must be a single character.")
     exit()
 count: int = 0
-print ("Searching for " + single + " in " + word)
-if str(word[0])== str(single):
+print("Searching for " + single + " in " + word)
+if str(word[0]) == str(single):
     print(str(single) + " found at index 0")
-    count = count+1
-if str(word[1])== str(single):
+    count = count + 1
+if str(word[1]) == str(single):
     print(str(single) + " found at index 1")
-    count = count +1
+    count = count + 1
 if str(word[2]) == str(single):
     print(str(single) + " found at index 2")
-    count = count+1
+    count = count + 1
 if str(word[3]) == str(single):
     print(str(single) + " found at index 3")
     count= count + 1
 if str(word[4]) == str(single):
     print(str(single) + " found at index 4")
-    count= count + 1
-if count ==0:
+    count = count + 1
+if count == 0:
     print("No instances of " + single + " found in " + word)
-if count ==1:
-     print("1 instance of " + single + " found in "+ word)
+if count == 1:
+    print("1 instance of " + single + " found in "+ word)
 else:
-     print (str(count) + " instances of " + single + " found in " + word)
+    print(str(count) + " instances of " + single + " found in " + word)
