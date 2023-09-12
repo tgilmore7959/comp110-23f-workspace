fafddee44837a536bdfcdf61a6fe38fcdfcc0a57
Tom Gilmore <tgilmore@unc.edu>
2023-09-12 19:01:04 -0400

    final

---
+++ lessons/conditionals/conditionals.py
@@ -0,0 +1,10 @@
+"""Testing conditionals with low card example"""
+
+low_card: int = 5
+current_card: int = 2
+
+if current_card < low_card:
+    low_card = current_card
+else:
+    print(str(current_card)+ "is not the low card")
+print("The low card is " + str(low_card))
---
+++ lessons/conditionals/guess.py
@@ -0,0 +1,7 @@
+"""Practice with guessing conditionals"""
+my_number_string: str = input("Guess a number: ")
+my_number: int = int(my_number_string)
+if my_number == int(10):
+    print(str("Right"))
+else:
+    print(str("Wrong"))
