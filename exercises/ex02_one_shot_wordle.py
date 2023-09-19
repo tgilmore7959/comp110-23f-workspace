95106e857404848c037211442d8b3ac8d12a2737
Thomas Gilmore <tgilmore@unc.edu>
2023-09-19 14:51:36 -0400

    asdf

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,29 +1,29 @@
 """One Shot Wordle."""
 __author__ = "730585444"
-#I wrote a version that got to the yellow box step and absolutly didn't work...rewriting with
-#the knowlege that I'll have to change number of letters etc =)
+# I wrote a version that got to the yellow box step and absolutly didn't work...rewriting with
+# the knowlege that I'll have to change number of letters etc =)
 # Setting word to guess
 secret_word = "python"
-#variables for emojis and number of letters
+# variables for emojis and number of letters
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 number_of_letters = 6
-#prompt and set var guess
+# prompt and set var guess
 guess = input (f"What is your {number_of_letters}-letter guess? ")
-#is it less than
+# is it less than
 while len(guess) != number_of_letters:
     print(f"That was not {number_of_letters} letters! Try again: ")
     guess = input(f"What is your {number_of_letters}-letter guess? ")
-#variable for emoji box output
+# variable for emoji box output
 result = ""
-#checking for matching (green)
+# checking for matching (green)
 index = 0
 while index < number_of_letters:
     if guess[index] == secret_word[index]:
         result += GREEN_BOX + " "
-#loop to search all index and if present it gets a yellow box  if false it gets white box then it
-#goes back to increase index and repeat ind search.
+# loop to search all index and if present it gets a yellow box  if false it gets white box then it
+# goes back to increase index and repeat ind search.
     else:
         ind = 0
         present = False
@@ -36,9 +36,9 @@ while index < number_of_letters:
         else:
             result += WHITE_BOX + " "
     index += 1
-#printing result emoji boxes
+# printing result emoji boxes
 print(result)
-#right or wrong
+# right or wrong
 if guess == secret_word:
     print("Woo! You got it!")
     exit()
