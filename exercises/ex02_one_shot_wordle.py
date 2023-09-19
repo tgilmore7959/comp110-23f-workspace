5a8c85e312046122e483d428b0a095a77908a65d
Thomas Gilmore <tgilmore@unc.edu>
2023-09-19 13:59:08 -0400

    starting over

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,21 +1,11 @@
-### One Shot Wordle ###
+""" One Shot Wordle """
 __author__ = "730585444"
-###Setting word to guess
+# Setting word to guess
 secret_word = "python"
-###variables for emojis
+#variables for emojis and guess input
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-###is it 6 letters?
-while True:
-    guess: str = input("What is your 6-letter guess? ")
-    if len(guess) == 6:
-        break
-    else: 
-        print("That was not 6 letters! Try again: ")
-if guess[0] == secret_word[0]:
-    print(GREEN_BOX)
-if str(guess) == secret_word:
-    print("Woo! You got it!")
-else:
-    print("Not Quite. Play again soon!")    
+number_of_letters = 6
+#prompt and set var guess
+guess = input (f"What is your {number_of_letters}-letter guess? ")
