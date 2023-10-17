38ba4259499d6c4253b9f9e309759d935a635b7a
Thomas Gilmore <tgilmore@unc.edu>
2023-10-17 19:03:38 -0400

    asdf

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,8 +1,9 @@
 """Summing the elements of a list using different loops."""
-___author___ = 730585444
+__author__ = 730585444
 
 
 def w_sum(vals: list[float]) -> float:
+    """While index to add up list."""
     sum = 0.0
     index = 0
     while index < len(vals):
@@ -12,12 +13,15 @@ def w_sum(vals: list[float]) -> float:
 
 
 def f_sum(vals: list[float]) -> float:
+    """For loop to add up list."""
     sum2 = 0.0
     for elem in vals:
        sum2 += elem
     return sum2
 
+
 def f_range_sum(vals: list[float]) -> float:
+    """Range list add list."""
     sum3 = 0.0
     for index in range(len(vals)):
         sum3 += vals[index]
