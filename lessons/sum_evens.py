58080cd4fb78bcfdac466e76f5b1c68101b7f10f
Tom Gilmore <tgilmore@unc.edu>
2023-10-26 19:27:44 -0400

    asdf

--- lessons/sum_evens.py
+++ lessons/sum_evens.py
@@ -3,6 +3,8 @@
 
 def sum_evens_in_list(input_list: list[int]) -> int:
     """Adds up th even numbers."""
+    list_sum: int = 0
     for number in input_list:
-        list_sum = 0
+        if number % 2 == 0:
+            list_sum = list_sum += number
     return list_sum  
--- lessons/sum_evens_test.py
+++ lessons/sum_evens_test.py
@@ -2,6 +2,12 @@
 
 from lessons.sum_evens import sum_evens_in_list
 
-def test_empty_sum()-> None:
+def test_empty_sum() -> None:
     """sum_evens_in_list of empty list should be 0."""
     assert sum_evens_in_list([]) == 0
+
+def test_list_length_1() -> None:
+    """Testing on a list of one element"""
+    test_list: list[int] = [2]
+    assert sum_evens_in_list == 2
+    
