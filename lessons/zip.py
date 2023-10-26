d8b00bf31fcf92c1e037a941b41ae39e775604ae
Thomas Gilmore <tgilmore@unc.edu>
2023-10-26 18:28:36 -0400

    asdf

--- lessons/zip.py
+++ lessons/zip.py
@@ -2,9 +2,11 @@
 __author__ = "730585444"
 
 
-def zip(input_list1: [str], input_list2: [int]) -> dict([str], [int]):
-    """Input two lists, get a dict"""
+def zip(input_list1: list[str], input_list2: list[int]) -> dict[str, int]:
+    """Input two lists, get a dict."""
     zipdict: dict[str, int] = {}
+    if len(input_list1) == 0 or len(input_list2) == 0:
+        return zipdict
     if len(input_list1) != len(input_list2):
         return zipdict
     for item1, item2 in zip(input_list1, input_list2):
