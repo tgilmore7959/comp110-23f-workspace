86d45bb8d819286471f000de27dc0da62599d0e6
Tom Gilmore <tgilmore@unc.edu>
2023-11-03 23:24:42 -0400

    asdf

---
+++ lessons/classes/pizza.py
@@ -0,0 +1,11 @@
+"""Defining a Class!"""
+
+
+class Pizza:
+    #attributes
+    # Think of these as variables each instance of my class will have
+    # <name> : <type>
+    size: str
+    toppings: int
+    gluten_free: bool
+    
---
+++ lessons/classes/pizza_orders.py
@@ -0,0 +1,12 @@
+"""Instantiating A Class"""
+
+
+from lessons.classes.pizza import Pizza
+
+my_pizza: Pizza = Pizza() #Constructor
+my_pizza.size = "large"
+my_pizza.toppings = 42
+my_pizza.gluten_free = False
+
+# printing out some values
+print(my_pizza)
